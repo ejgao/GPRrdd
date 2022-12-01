e24cacb1e37416094e0bd3b7cde5a7b27e0b314b
Eric Gao <ejgao@ucdavis.edu>
2022-11-30 23:59:44 -0600

    added command to calculate kernel between a single point and a vector inside of gp_posterior function

--- R/GPprior.R
+++ R/GPprior.R
@@ -4,7 +4,7 @@
 #' @param X matrix input
 #' @param Y vector input
 #' @param b scalar input should be the row number user wants to remove(has to be greater than 2)
-#' @param degree scalar input, default is 0 if no argument given
+#' @param degree scalar input, default is NULL if no argument given
 #' @param sigma_hat vector input
 #' @param choice scalar input 1 or 2, default is 1
 #' @param l vector input
@@ -17,7 +17,7 @@
 #' X <- matrix(rnorm(24), nrow = 4)
 #' y <- c(1, 2, 3, 4)
 #' gp_prior(X, y, b = 3, sigma_hat = 0.3, l = 2)
-gp_prior <- function(X, Y, b, degree = 0, sigma_hat, choice = 1, l = NULL, alpha = NULL) {
+gp_prior <- function(X, Y, b, degree = NULL, sigma_hat, choice = 1, l = NULL, alpha = NULL) {
   # y is response, x is matrix of predictors
   # user can specify the degree here
   # first need to split the?plo data by a boundary point/discontinuity point
@@ -26,9 +26,9 @@ gp_prior <- function(X, Y, b, degree = 0, sigma_hat, choice = 1, l = NULL, alpha
   Xc <- X[(1:b - 1), ]
   Yc <- Y[1:b - 1]
   # may have to do regression on one column of X, not every column but need to check on this
-  if (degree == 0) {
-    mean_function_control <- rep(0, ncol(Xc))
-    mean_function_treatment <- rep(0, ncol(Xc))
+  if (is.null(degree) == TRUE) {
+    mean_function_control <- rep(0, length(Xc))
+    mean_function_treatment <- rep(0, length(Xt))
   } else {
     # mean function for control
     mean_function_control <- mean_function(X, Y, degree)
@@ -45,7 +45,7 @@ gp_prior <- function(X, Y, b, degree = 0, sigma_hat, choice = 1, l = NULL, alpha
     K <- rational_quad_kernel(Xc, alpha, l, sigma_hat)
   }
   # return means, K
-  return(list(mean = mean_function_control, K = K))
+  return(list(mean_control= mean_function_control, mean_treatment = mean_function_treatment, K = K))
 }
 
 # mean function
@@ -53,7 +53,8 @@ mean_function = function(X, Y, degree){
   mean_func = lm(Y ~ poly(x, degree = degree, raw = TRUE))$coefficients
 }
 
-squared_exponential_covfunction <- function(b, X, sigma_hat, l) {
+# squared exponnential covariance kernel
+squared_exponential_covfunction <- function(b = NULL, X, sigma_hat, l) {
   # b is the discontinuity point
   # for now, allow users to pick sigma_hat, l, will optimize if time permits
   # allow users to choose which function they want to pick
@@ -81,7 +82,8 @@ squared_exponential_covfunction <- function(b, X, sigma_hat, l) {
 }
 
 
-rational_quad_kernel <- function(X, alpha, l, sigma_hat) {
+# rational quadratic kernel
+rational_quad_kernel <- function(b = NULL, X, alpha, l, sigma_hat) {
   n <- length(X)
   if (is.null(b) == TRUE){
     for (i in 1:n) {
@@ -96,7 +98,8 @@ rational_quad_kernel <- function(X, alpha, l, sigma_hat) {
       K[i] = sigma_hat * (1 + (b[i] - X[i])^2 / (2 * alpha * l^2))^(-alpha)
     }
   }
-  # if b is supplied, find kernel between b and k(1 by n)
+  # if b is supplied, find kernel between b and X(1 by n)
+  # cov between X and b is n by 1
   return(K)
 }
 
@@ -105,32 +108,39 @@ gp_posterior <- function(X, Y, b, sigma_hat, choice = 1, l = NULL, alpha = NULL)
   # b is boundary point. it is a scalar, let b the row number you want to let be the boundary
   # mean function at b
   # Xt is stuff after the boundary
-  Xc <- X[1:(b - 1)]
+  Xcc <- X[1:(b - 1)]
   Xt <- X[(b + 1):nrow(n), ]
   Yc <- Y[1:b - 1]
   Yt <- Y[(b + 1):nrow(n)]
   n <- nrow(Xt)
-  sigma_y <- var(Y) * diag(n)
+  # sigma_y <- var(Y) * diag(n)
   if (choice == 1) {
     Kc <- squared_exponential_covfunction(Xc, sigma_hat, l)
     Kt <- squared_exponential_covfunction(Xt, sigma_hat, l)
-
+    Kb_xc <- squared_exponential_covfunction(b, Xc, sigma_hat, l)
+    Kb_xt <- squared_exponential_covfunction(b, Xt, sigma_hat, l)
   }
   if (choice == 2) {
     Kc <- rational_quad_kernel(Xc, alpha, l, sigma_hat)
     Kt <- rational_quad_kernel(Xt, alpha, l, sigma_hat)
+    Kb_xc <- rational_quad_kernel(b, Xc, sigma_hat, l)
+    Kb_xt <- rational_quad_kernel(b, Xt, sigma_hat, l)
   }
-  need to get posterior mean for both treatment and control
+  #need to get posterior mean for both treatment and control
   diff_t = Yt - mean_function(Xt, Yt, degree)
   diff_c = Yc - mean_function(Xc, Yc, degree)
-  kernel_t = e + solve(Kt + sigma_y)
-  kernel_c = KTc + solve(Kc + sigma_y)
-  posterior mean is given by mean at the boundary + kernel*(y-mean)
-  post_mean_c = 0 + "quatty" + kernel_t
-  post_mean_treat = 0 + "quatty" + kernel_c
+  kernel_t = Kb_xt + solve(Kt + var(y) + diag(length(Yt)))
+  kernel_c = Kb_xc + solve(Kc + var(y) + diag(length(Yc)))
+  #posterior mean is given by mean at the boundary + kernel*(y-mean)
+  # case when mean is 0 first
+  post_mean_c = 0 + (kernel_c %*% diff_c)
+  post_mean_t = 0 + (kernel_t %*% diff_t)
   # next get posterior variance
-
+  posterior_kc = kernel_c %*% t(kb_xc)
+  posterior_kt = kernrel_t %*% t(kb_xt)
   # need to get posterior variance for both treatment and control
+  return(list(posterior_c_mean = post_mean_c, posterior_t_mean = post_mean_t, posterior_c_var = posterior_kc,
+              posterior_t_var = posterior_kt))
 }
 
 
@@ -142,7 +152,7 @@ gp_posterior <- function(X, Y, b, sigma_hat, choice = 1, l = NULL, alpha = NULL)
 #'
 #' @export
 #' @examples
-create_plot <- function(X, Y, b, choice) {
+create_plot <- function(X, Y, b, degree, choice, sigma_hat, alpha) {
   # return plot
   # return gp prior plot
   # create prior plot with boundary removed
@@ -158,4 +168,8 @@ create_plot <- function(X, Y, b, choice) {
   posterior <- gp_posterior(X, Y, b, choice, sigma_hat, alpha)
   # plot
   # return plot
+  prior_mean = prior$mean
+  post_mean = posterior
+  # plot posterior mean
+  plot(1)
 }
